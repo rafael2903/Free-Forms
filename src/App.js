b834299b8bea69237ae87e74ede0d2d6ffacc999
Duda-Alves18 <dudasta19@gmail.com>
2021-04-20 17:58:12 -0300

    criar header

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Header from './components/Header';
 
 import GlobalStyle from './GlobalStyles';
 
 function App() {
-  const [loggedIn, setloggedIn] = useState(() => JSON.parse(localStorage.getItem('loggedIn')));
+    const [loggedIn, setloggedIn] = useState(() => JSON.parse(localStorage.getItem('loggedIn')));
 
-  const [id, setId] = useState(() => localStorage.getItem('id'));
+    const [id, setId] = useState(() => localStorage.getItem('id'));
 
-  const [admin, setAdmin] = useState(() => localStorage.getItem('admin'));
+    const [admin, setAdmin] = useState(() => localStorage.getItem('admin'));
 
-  return (
-    <div className="App">
-      <GlobalStyle />
-
-      <Router>
-        <Switch>
-          {/* <Header /> */}
-          <Route exact path="/">
-            <h1>oiee</h1>
-          </Route>
-
-          {/* <Route path="/login">
-            {loggedIn ? (
-              <Redirect to="/" />
-            ) : (
-              <Login setloggedIn={setloggedIn} setId={setId} setAdmin={setAdmin} />
-            )}
-          </Route> */}
-
-          {/* <Route path="/signup">{loggedIn ? <Redirect to="/" /> : <SignUp />}</Route> */}
-        </Switch>
-      </Router>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        GlobalStyle / >
+        <
+        Router >
+        <
+        Switch >
+        <
+        Header / >
+        <
+        Route exact path = "/" >
+        <
+        h1 > oieee < /h1>{' '} <
+        /Route>{' '} {
+            /* <Route path="/login">
+                                                            {loggedIn ? (
+                                                              <Redirect to="/" />
+                                                            ) : (
+                                                              <Login setloggedIn={setloggedIn} setId={setId} setAdmin={setAdmin} />
+                                                            )}
+                                                          </Route> */
+        } { ' ' } { /* <Route path="/signup">{loggedIn ? <Redirect to="/" /> : <SignUp />}</Route> */ } { ' ' } <
+        /Switch>{' '} <
+        /Router>{' '} <
+        /div>
+    );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,6 @@
+import { StyledHeader } from './style';
+
+function Header() {
+    return <StyledHeader / > ;
+}
+export default Header;
