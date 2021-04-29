5c8ac35a2b1c65304fa1900d3ab66a815d03edbc
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2021-04-29 19:36:18 -0300

    Update styles.js

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   width: 50rem;
-  height: 50rem;
+  height: auto;
   margin: 5rem auto;
   box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.3);
   border-radius: 0.3rem;
